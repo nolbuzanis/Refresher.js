e56513df6f209eba6a2bc509c7fedc30790531a8
nolbuzanis <nolanbuzanis@gmail.com>
2019-10-09 21:18:34 -0400

    Spotify oauth working. Session are disabled for now.

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const passport = require('passport');
-const SpotifyStrategy = require('passport-spotify');
+const SpotifyStrategy = require('passport-spotify').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
 passport.use(
   new SpotifyStrategy(
     {
-      clientID: '',
-      clientSecret: '',
+      clientID: keys.spotifyClientID,
+      clientSecret: keys.spotifyClientSecret,
       callbackURL: '/auth/spotify/callback'
     },
-    (accessToken, refreshToken, expires_in, profile, done) => {}
+    (accessToken, refreshToken, expires_in, profile, done) => {
+      try {
+        console.log(profile);
+
+        if (!profile) {
+          done(null, false);
+        }
+        done(null, profile);
+      } catch (err) {
+        console.log(err);
+        done(err);
+      }
+    }
   )
 );
+
+app.get('/auth/spotify', passport.authenticate('spotify', { session: false }));
+app.get(
+  '/auth/spotify/callback',
+  passport.authenticate('spotify', { session: false }),
+  (req, res) => {
+    res.send(req.user);
+  }
+);
+
+app.get('/', (req, res) => {
+  res.send("<a href='/auth/spotify' >Log In With Spotify</a>");
+});
+
+app.listen('8000');
